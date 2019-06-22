b47e61024cc4eb5284f2e7ba0f3032c7bf82d3ad
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2019-06-22 17:03:28 +0100

    Implementation finished - time to point Makefile to the main method classes

--- src/main/java/pho/findingsandiego/functional/finder/BurglarFinderError.java
+++ src/main/java/pho/findingsandiego/functional/finder/BurglarFinderError.java
@@ -1,4 +1,10 @@
 package pho.findingsandiego.functional.finder;
 
 public class BurglarFinderError {
+
+    public static void main(String[] args) {
+        BurglarPredicateBuilder
+            .newBurglarPredicate()
+            .withGender(null);
+    }
 }
